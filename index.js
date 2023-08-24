0a81e5c8ebdfe02f7d43ad0b19bccb89e34dfe6e
Michał Bidziński <michal.bidzinski1337@gmail.com>
2023-08-24 14:21:20 +0200

    code update

--- index.js
+++ index.js
@@ -1,97 +1,127 @@
 const express = require("express");
-const app = express();
 const axios = require("axios");
-const PORT = 3000;
-var cors = require("cors");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const token = process.env.GITHUB_TOKEN;
+
 app.use(cors());
 app.use(express.json());
-require("dotenv").config();
-const token = process.env.GitHub_Token;
 
-// routes
-app.get("/:name", (req, res) => {
-  getRepositoryNames(req.params.name).then((repositories) => {
-    getRepositoryLanguages(req.params.name, repositories).then(
-      (languageStats) => {
-        res.send(languageStats);
-      }
-    );
-  });
+app.get("/favicon.ico", (req, res) => {
+  res.status(204).end();
 });
 
+app.get("/:name", getUserLanguageStats);
+
+// Cached response storage
+const cachedResponses = new Map();
+
+async function getUserLanguageStats(req, res) {
+  const username = req.params.name;
+  try {
+    const repositories = await getRepositoryNames(username);
+    if (!repositories.length) {
+      return res.send("There is no user with such name!");
+    }
+
+    const cachedStats = cachedResponses.get(username);
+    if (cachedStats) {
+      return res.send(cachedStats);
+    }
+
+    const languageStats = await getLanguageStatsForRepositories(username, repositories);
+    if (Object.keys(languageStats.languages).length === 0) {
+      return res.send("This user has empty repositories!");
+    }
+
+    calculateLanguagePercentages(languageStats);
+    
+    cachedResponses.set(username, languageStats); // Cache the response
+
+    res.send(languageStats);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred.");
+  }
+}
+
 async function getRepositoryNames(username) {
-  return await axios
-    .get(`https://api.github.com/users/${username}/repos`, {
+  try {
+    const response = await axios.get(`https://api.github.com/users/${username}/repos`, {
       headers: {
         Authorization: `Bearer ${token}`,
         "Content-Type": "application/json",
       },
-    })
-    .then((response) => {
-      return response.data
-        .map((repo) => ({
-          name: repo.name,
-        }))
-        .map((e) => e.name);
-    })
-    .catch((error) => {
-      return [];
     });
-}
 
-async function getRepositoryLanguages(owner, repos) {
-  let languageStats = [];
-  if (repos.length == 0) {
-    return "There is no user with such name!";
-  } else {
-    for (let index = 0; index < repos.length; index++) {
-      const repoName = repos[index];
-
-      const response = await axios.get(
-        `https://api.github.com/repos/${owner}/${repoName}/languages`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      languageStats.push({
-        repoName: repoName,
-        languages: response.data,
-      });
-    }
-
-    const res = languageStats.reduce(
-      (a, b) => {
-        a.repositories.push(b.repoName);
-        for (const lang of Object.keys(b.languages)) {
-          if (a.languages.hasOwnProperty(lang)) {
-            a.languages[lang] += b.languages[lang];
-          } else {
-            a.languages[lang] = b.languages[lang];
-          }
-        }
-        return a;
-      },
-      { languages: {}, repositories: [] }
-    );
-
-    if (Object.keys(res.languages) == 0) {
-      return "This user has empty repositories!";
-    } else {
-      const totBytes = Object.values(res.languages).reduce((a, b) => a + b);
-
-      for (const lang of Object.keys(res.languages)) {
-        res.languages[lang] =
-          ((res.languages[lang] / totBytes) * 100).toFixed(1) + "%";
-      }
-
-      return res;
-    }
+    return response.data.map(repo => repo.name);
+  } catch (error) {
+    return [];
   }
 }
+
+async function getLanguageStatsForRepositories(owner, repos) {
+  const languageStats = {
+    languages: {},
+    totalBytes: 0,
+  };
+
+  const requests = repos.map(async repoName => {
+    try {
+      const response = await axios.get(`https://api.github.com/repos/${owner}/${repoName}/languages`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
+
+      updateLanguageStats(languageStats, response.data);
+    } catch (error) {
+      console.error(`Error fetching languages for ${owner}/${repoName}:`, error);
+    }
+  });
+
+  await Promise.all(requests);
+
+  return languageStats;
+}
+
+function updateLanguageStats(languageStats, data) {
+  for (const lang of Object.keys(data)) {
+    languageStats.languages[lang] = (languageStats.languages[lang] || 0) + data[lang];
+    languageStats.totalBytes += data[lang];
+  }
+}
+
+function calculateLanguagePercentages(languageStats) {
+  const totalBytes = languageStats.totalBytes;
+
+  const filteredLanguages = {};
+  for (const lang of Object.keys(languageStats.languages)) {
+    const percentage = (languageStats.languages[lang] / totalBytes) * 100;
+    if (percentage > 1) {
+      filteredLanguages[lang] = percentage.toFixed(1) + "%";
+    }
+  }
+
+  const sortedLanguages = Object.keys(filteredLanguages).sort((a, b) => {
+    const percentageA = parseFloat(filteredLanguages[a]);
+    const percentageB = parseFloat(filteredLanguages[b]);
+    return percentageB - percentageA;
+  });
+
+  const sortedLanguageStats = {};
+  for (const lang of sortedLanguages) {
+    sortedLanguageStats[lang] = filteredLanguages[lang];
+  }
+
+  languageStats.languages = sortedLanguageStats;
+}
+
+
 app.listen(PORT, () => {
   console.log(`API server listening at http://localhost:${PORT}`);
 });
